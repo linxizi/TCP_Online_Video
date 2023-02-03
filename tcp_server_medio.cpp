318c809194a49b4a2c2710287de9e70a968f80fa
linxizi <925762221@qq.com>
2023-02-03 15:40:34 +0800

    Add files via upload

---
+++ tcp_server_medio.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include "server.h"
+#pragma comment(lib,"opencv_world340.lib")//引用库文件
+
+int recv_online_video() {
+	Server ser;
+	int port = 7777;
+	ser.initialization(port);
+	ser.build_connect();
+	ser.receive_data();
+	ser.free_connect();
+	return 0;
+}
+
+int recv_pic()
+{
+	Server ser;
+	int port = 7777;
+	ser.initialization(port);
+	ser.build_connect();
+	Mat output;
+	ser.receive_data_frame(output);
+	imshow("s", output);
+	waitKey(0);
+
+	ser.free_connect();
+	return 0;
+}
+
+int main() {
+	recv_online_video();
+	//recv_pic();
+	return 0;
+}
+
+
